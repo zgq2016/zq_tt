b9b69959129e0bbb815bc8452aea3b34295de09e
zgq <zgq>
2019-12-20 15:35:03 +0800

    03.个人中心静态页面完成

--- src/views/personal.vue
+++ src/views/personal.vue
@@ -1,5 +1,67 @@
 <template>
   <div class="personal">
-personal
+    <router-link to="/">
+      <div class="header">
+        <div class="left">
+          <img src="../assets/logo.png" alt />
+          <div>
+            <div class="name iconfont iconxingbienan">火星网友</div>
+            <div class="time">2019-10-10</div>
+          </div>
+        </div>
+        <div class="right">
+          <van-icon name="arrow" />
+        </div>
+      </div>
+    </router-link>
+    <van-cell title="我的关注" is-link value="关注的用户" />
+    <van-cell title="我的跟帖" is-link value="跟帖/回复" />
+    <van-cell title="我的收藏" is-link value="文章/视频" />
+    <van-cell title="设置" is-link />
   </div>
 </template>
+
+<style lang="less" scoped>
+.personal {
+    background-color: #eee;
+  .header {
+    padding: 30vw * 100/360 0;
+    display: flex;
+    border-bottom: 5px solid #bbb;
+    .left {
+      flex: 1;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      img {
+        width: 70px;
+        height: 70px;
+        margin-right: 10px;
+        border-radius: 50%;
+      }
+      div {
+        .name {
+          font-size: 14vw * 100/360;
+        }
+        .time {
+          margin-top: 5px;
+          font-size: 10vw * 100/360;
+          color: #bbb;
+        }
+      }
+    }
+    .right {
+      flex: 1;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      .van-icon-arrow {
+        padding: 10vw * 100/360;
+      }
+    }
+  }
+  .van-cell--clickable{
+    border-bottom: 1px solid #ccc;
+  }
+}
+</style>
