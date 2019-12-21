71d0ed4d7b1ed59e8d0a19fd98eeb5c4f2dea1eb
zgq2016 <zgq>
2019-12-21 18:31:02 +0800

    06.编辑密码

--- src/views/editdata.vue
+++ src/views/editdata.vue
@@ -8,13 +8,32 @@
       <van-uploader :after-read="afterRead" />
     </div>
     <!-- 数据 -->
-    <van-cell title="昵称" is-link :value="users.nickname" @click="nicknameShow=!nicknameShow" />
-    <van-cell title="密码" is-link :value="users.password" />
-    <van-cell title="性别" is-link :value="users.gender===1?'男':'女'" />
+    <van-cell title="昵称" is-link :value="users.nickname" @click="nickShow=!nickShow" />
+    <van-cell title="密码" is-link @click="passShow=!passShow" value="*****"></van-cell>
+    <!-- <van-cell title="密码" is-link type='password' :value="users.password" @click="passShow=!passShow" /> -->
+    <van-cell title="性别" is-link :value="users.gender===1?'男':'女'" @click="genderShow=!genderShow" />
     <!-- 弹框 -->
     <!-- nickname -->
-    <van-dialog v-model="nicknameShow" title="编辑昵称" show-cancel-button @confirm='updateNick'>
-      <van-field :value='users.nickname' placeholder="编辑昵称" ref="nickname" />
+    <van-dialog v-model="nickShow" title="编辑昵称" show-cancel-button @confirm="updateNick">
+      <van-field :value="users.nickname" placeholder="编辑昵称" ref="nickname" />
+    </van-dialog>
+    <!-- password -->
+    <van-dialog
+      v-model="passShow"
+      title="修改密码"
+      show-cancel-button
+      @confirm="updatePass"
+      :before-close="beforeClose"
+    >
+      <van-field placeholder="旧密码" ref="oldpass" />
+      <van-field placeholder="新密码" ref="newpass" />
+    </van-dialog>
+    <!-- gender -->
+    <van-dialog v-model="genderShow" title="选择性别" show-cancel-button @confirm="updateGender" width='200'>
+      <van-radio-group v-model="radio">
+        <van-radio label-position='left' name="0">女</van-radio>
+        <van-radio label-position='left' name="1">男</van-radio>
+      </van-radio-group>
     </van-dialog>
   </div>
 </template>
@@ -26,22 +45,67 @@ import { uploadFile } from '@/api/uploadFile.js'
 export default {
   data () {
     return {
+      id: '',
       users: {},
-      nicknameShow: false
+      nickShow: false,
+      passShow: false,
+      genderShow: false,
+      radio: '1'
     }
   },
   methods: {
+    // 性别
+    updateGender () {
+
+    },
+    // 修改密码取消事件
+    beforeClose (action, done) {
+      let oldPwd = this.$refs.oldpass.$refs.input.value
+      let password = this.$refs.newpass.$refs.input.value
+      // 2.判断输入的原密码是否和真正的原始密码一致
+      // 只有单击了确定的时候才需要让原始密码输入正确
+      if (action === 'confirm' && this.users.password !== oldPwd) {
+        this.$toast.fail('原始密码输入不正确')
+        done(false)
+      } else if (action === 'confirm' && !/\w{3,16}/.test(password)) {
+        this.$toast.fail('请输入3~16位的新密码')
+        done(false)
+      } else {
+        done()
+      }
+    },
+    // 修改密码
+    async updatePass () {
+      // console.log(this.$refs.oldpass.$refs.input.value)
+      let oldpass = this.$refs.oldpass.$refs.input.value
+      if (this.users.password === oldpass) {
+        let password = this.$refs.newpass.$refs.input.value
+        if (!/\w{3,16}/.test(password)) {
+          this.$toast.fail('请输入3~16位的新密码')
+          return
+        }
+        let res = await userUpdate(this.id, { password })
+        console.log(res)
+        if (res.data.message === '修改成功') {
+          this.$toast.success(res.data.message)
+          localStorage.clear()
+          this.$router.push({ name: 'Login' })
+        }
+      }
+    },
+    // 编辑昵称
     async updateNick () {
-      let id = localStorage.getItem('zq_id')
+      // let id = localStorage.getItem('zq_id')
       // console.log(this.$refs.nickname.$refs.input.value)
       let nickname = this.$refs.nickname.$refs.input.value
-      let res = await userUpdate(id, { nickname })
+      let res = await userUpdate(this.id, { nickname })
       // console.log(res)
       if (res.data.message === '修改成功') {
         this.$toast.success(res.data.message)
         this.users.nickname = res.data.data.nickname
       }
     },
+    // 上传图片
     async afterRead (file) {
       // console.log(file)
       let formdata = new FormData()
@@ -49,8 +113,8 @@ export default {
       let res = await uploadFile(formdata)
       // console.log(res)
       if (res.data.message === '文件上传成功') {
-        let id = localStorage.getItem('zq_id')
-        let res1 = await userUpdate(id, { head_img: res.data.data.url })
+        // let id = localStorage.getItem('zq_id')
+        let res1 = await userUpdate(this.id, { head_img: res.data.data.url })
         if (res1.data.message === '修改成功') {
           this.$toast.success(res1.data.message)
           this.users.head_img =
@@ -63,10 +127,11 @@ export default {
       }
     }
   },
+  // 默认数据
   async mounted () {
-    let id = localStorage.getItem('zq_id')
+    this.id = localStorage.getItem('zq_id')
     let baseurl = localStorage.getItem('baseurl')
-    let res = await userDetail(id)
+    let res = await userDetail(this.id)
     // console.log(res)
     this.users = res.data.data
     if (this.users.head_img) {
@@ -102,6 +167,10 @@ export default {
       transform: translate(-50%, -50%);
       opacity: 0;
     }
+  }
+  /deep/.van-icon-success{
+    border: 1px solid #000;
+    border-radius: 50%;
   }
 }
 </style>
