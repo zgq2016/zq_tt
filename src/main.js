ceff6de996f5ae3e633e78ee450ff32d1462c5e8
zgq2016 <zgq>
2019-12-22 12:05:52 +0800

    11.上拉加载+下拉刷新

--- src/main.js
+++ src/main.js
@@ -8,31 +8,31 @@ Vue.use(Vant)
 
 Vue.config.productionTip = false
 
-router.beforeEach((to, from, next) => {
-  let token = localStorage.getItem('zq_token')
-  if (token || to.path === '/login') {
-    next()
-  } else {
-    next({
-      name: 'Login'
-    })
-  }
-})
-
 // router.beforeEach((to, from, next) => {
-//   // 只有访问那些需要授权的api的时候，才需要进行守卫
-//   if (to.path.indexOf('/personal/') === 0) {
-//     // 判断当前用户是否登陆过
-//     let token = localStorage.getItem('zq_token')
-//     if (token) { // 如果有token数据，说明登陆过了
-//       next()
-//     } else { // 否则，没有登陆则重新云登陆--重定向
-//       next({ name: 'login' })
-//     }
-//   } else { // 如果访问不需要授权的页面，则自由访问
+//   let token = localStorage.getItem('zq_token')
+//   if (token || to.path === '/login') {
 //     next()
+//   } else {
+//     next({
+//       name: 'Login'
+//     })
 //   }
 // })
+
+router.beforeEach((to, from, next) => {
+  // 只有访问那些需要授权的api的时候，才需要进行守卫
+  if (to.path.indexOf('/personal/') === 0) {
+    // 判断当前用户是否登陆过
+    let token = localStorage.getItem('zq_token')
+    if (token) { // 如果有token数据，说明登陆过了
+      next()
+    } else { // 否则，没有登陆则重新云登陆--重定向
+      next({ name: 'login' })
+    }
+  } else { // 如果访问不需要授权的页面，则自由访问
+    next()
+  }
+})
 
 new Vue({
   router,
--- src/views/index.vue
+++ src/views/index.vue
@@ -6,45 +6,55 @@
       <div class="search" slot="title">
         <van-icon name="search" />搜索新闻
       </div>
-      <van-icon name="manager-o" slot="right" />
+      <van-icon name="manager-o" slot="right" @click="personal" />
     </van-nav-bar>
     <van-tabs v-model="active" sticky swipeable>
       <van-tab v-for="v1 in cateList" :key="v1.id" :title="v1.name">
-        <div v-for="v2 in v1.postList" :key="v2.id">
-          <div class="single" v-if="v2.cover.length <= 2 && v2.type===1">
-            <div class="left">
-              <p class="content">{{v2.title}}</p>
-              <p class="info">
-                <span>{{v2.user.nickname}}</span>
-                <span>{{v2.comment_length}}跟帖</span>
-              </p>
-            </div>
-            <img :src="v2.cover[0].url" alt />
-          </div>
-          <div class="single1" v-else-if="v2.cover.length > 2 && v2.type === 1">
-            <p class="content" style="padding: 0px 5px;">{{v2.title}}</p>
-            <div class="imgs">
-              <img v-for="item in v2.cover" :key="item.id" :src="item.url" alt />
-            </div>
-            <p class="info">
-              <span>{{v2.user.nickname}}</span>
-              <span>{{v2.comment_length}}跟帖</span>
-            </p>
-          </div>
-          <div class="single2" v-else-if="v2.type === 2">
-            <p class="content" style="padding: 0px 5px;">{{v2.title}}</p>
-            <div class="myvideo">
-              <img :src="v2.cover[0].url" alt />
-              <div class="playbtn">
-                <van-icon name="play" />
+        <van-list
+          :immediate-check="false"
+          :offset="10"
+          v-model="v1.loading"
+          :finished="v1.finished"
+          finished-text="没有更多了"
+          @load="onLoad">
+          <van-pull-refresh v-model="v1.isLoading" @refresh="onRefresh">
+            <div v-for="v2 in v1.postList" :key="v2.id">
+              <div class="single" v-if="v2.cover.length <= 2 && v2.type===1">
+                <div class="left">
+                  <p class="content">{{v2.title}}</p>
+                  <p class="info">
+                    <span>{{v2.user.nickname}}</span>
+                    <span>{{v2.comment_length}}跟帖</span>
+                  </p>
+                </div>
+                <img :src="v2.cover[0].url" alt />
+              </div>
+              <div class="single1" v-else-if="v2.cover.length > 2 && v2.type === 1">
+                <p class="content" style="padding: 0px 5px;">{{v2.title}}</p>
+                <div class="imgs">
+                  <img v-for="item in v2.cover" :key="item.id" :src="item.url" alt />
+                </div>
+                <p class="info">
+                  <span>{{v2.user.nickname}}</span>
+                  <span>{{v2.comment_length}}跟帖</span>
+                </p>
+              </div>
+              <div class="single2" v-else-if="v2.type === 2">
+                <p class="content" style="padding: 0px 5px;">{{v2.title}}</p>
+                <div class="myvideo">
+                  <img :src="v2.cover[0].url" alt />
+                  <div class="playbtn">
+                    <van-icon name="play" />
+                  </div>
+                </div>
+                <p class="info">
+                  <span>{{v2.user.nickname}}</span>
+                  <span>{{v2.comment_length}}跟帖</span>
+                </p>
               </div>
             </div>
-            <p class="info">
-              <span>{{v2.user.nickname}}</span>
-              <span>{{v2.comment_length}}跟帖</span>
-            </p>
-          </div>
-        </div>
+          </van-pull-refresh>
+        </van-list>
       </van-tab>
     </van-tabs>
   </div>
@@ -61,15 +71,46 @@ export default {
     }
   },
   methods: {
+    personal () {
+      this.$router.push({ name: 'Personal' })
+    },
     async getPostList () {
-      let res1 = await getArticleList({
+      let res = await getArticleList({
         pageIndex: this.cateList[this.active].pageIndex,
         pageSize: this.cateList[this.active].pageSize,
         category: this.cateList[this.active].id
       })
-      console.log(res1)
-      this.cateList[this.active].postList = res1.data.data
-      // console.log(res1.data.data[0].cover.length)
+      // console.log(res)
+      // this.cateList[this.active].postList = res.data.data
+      this.cateList[this.active].postList.push(...res.data.data)
+      // 本次获取了数据之后，将loading重置为false
+      this.cateList[this.active].loading = false
+      // 重置isLoading,让下拉刷新的提示消失
+      this.cateList[this.active].isLoading = false
+      // 判断是否数据已经完全加载完毕
+      if (res.data.data.length < this.cateList[this.active].pageSize) {
+        this.cateList[this.active].finished = true
+      }
+    },
+    onLoad () {
+      // 加载下一页数据
+      // 1.页码加1
+      this.cateList[this.active].pageIndex++
+      // 2.根据新的页码参数获取数据
+      setTimeout(() => {
+        this.getPostList()
+      }, 3000)
+    },
+    onRefresh () {
+      this.cateList[this.active].pageIndex = 1
+      // 重置为False的作用就是让这个栏目可以重新的上拉加载更多数据
+      this.cateList[this.active].finished = false
+      setTimeout(() => {
+        // 清除数组的所有数据，可以避免反复的创建新的数组
+        this.cateList[this.active].postList.length = 0
+        // 获取数据
+        this.getPostList()
+      }, 2000)
     }
   },
   async mounted () {
@@ -82,7 +123,10 @@ export default {
           ...v,
           postList: [], // 新闻列表数据
           pageIndex: 1, // 页码
-          pageSize: 5 // 每页个数
+          pageSize: 5, // 每页个数
+          loading: false, // 当前栏目更多数据加载状态，默认为不在加载数据
+          finished: false, // 数据是否已经完毕加载完毕，默认为没有完毕
+          isLoading: false // 标记是否处于下拉刷新的状态，默认为false
         }
       })
       this.getPostList()
@@ -158,6 +202,12 @@ export default {
     }
   }
   .single1 {
+    padding: 15px 0px;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    border-bottom: 1px solid #ccc;
     .content {
       width: 100%;
       display: -webkit-box;
@@ -168,12 +218,13 @@ export default {
       -webkit-line-clamp: 2;
       -webkit-box-orient: vertical;
     }
-    padding: 15px 0px;
-    box-sizing: border-box;
-    display: flex;
-    justify-content: space-between;
-    flex-direction: column;
-    border-bottom: 1px solid #ccc;
+    .info {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 20px;
+      color: #999;
+      font-size: 12vw * 100/360;
+    }
     .imgs {
       flex: 1;
       display: flex;
